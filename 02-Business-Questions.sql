38c24804871ae41fc0d53f21687c7f89f2ccd266
Raul Rosa Pacheco <rr0608@outlook.com>
2022-11-23 12:50:27 -0300

    Update 02-Business-Questions.sql

--- 02-Business-Questions.sql
+++ 02-Business-Questions.sql
@@ -35,13 +35,17 @@ SELECT COUNT(*) FROM pandemic.covid_deaths;
 FROM pandemic.covid_deaths
 ORDER BY 2,1; # 2: location / 1: date
 
+# UNIVARIATE ANALYSIS
 # Average deaths and new cases by country
-# Univariate Analysis
-SELECT location, ROUND(AVG(total_deaths),2) avg_deaths
+SELECT 
+	location, 
+    ROUND(AVG(total_deaths),2) avg_deaths,
+    ROUND(AVG(new_cases),2) avg_new_cases
 FROM pandemic.covid_deaths
 GROUP BY location
 ORDER BY avg_deaths DESC;
 
+# MULTIVARIATE ANALYSIS
 # What is proportions of dealths in relation to the total number of cases in Brazil?
 SELECT
 	location,
@@ -52,7 +56,44 @@ SELECT
 FROM pandemic.covid_deaths
 WHERE location = 'Brazil'
 ORDER BY 2;
-    
+
+# What is average proportion between the total number of cases and the population of each location?
+SELECT
+	location,
+    AVG(total_cases/population)*100 avg_cases_per_population
+FROM pandemic.covid_deaths
+GROUP BY location
+ORDER BY avg_cases_per_population DESC;
+
+# Considering the highest value of total cases, 
+# Which countries have highest rate of infectios in relation to population?
+SELECT 
+	location,
+    MAX(total_cases) max_total_cases,
+	MAX((total_cases/population))*100 percentage_cases
+FROM pandemic.covid_deaths
+WHERE continent IS NOT NULL
+GROUP BY location
+ORDER BY percentage_cases DESC;
+	
+# Which location have the highest number of deaths?
+SELECT
+	location,
+    MAX(total_deaths*1) total_deaths # The total_deaths column id of type text, when multiplied by 1 the SGBD will return a numeric value
+FROM pandemic.covid_deaths
+WHERE continent IS NOT NULL
+GROUP BY location
+ORDER BY total_deaths DESC;
+
+# Solving the previous problem optimally
+SELECT location,
+	MAX(CAST(total_deaths AS UNSIGNED)) total_deaths
+FROM pandemic.covid_deaths
+WHERE continent IS NOT NULL
+GROUP BY location
+ORDER BY total_deaths DESC;
+
+
 
  
  
